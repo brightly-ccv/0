97677e930538f9e9d326ab0c0f64be603b12cf8f
brightly <brightly-ccv>
2022-12-06 14:13:31 +0000

    initial installer script

---
+++ installer.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install ansible firefox keepassxc -y
+sudo hostnamectl hostname nllt-100701
+sudo ssh-keygen -b 4096
+cat ~/.ssh/id_rsa.pub
+firefox https://github.com/settings/keys
+mkdir ~/Documents/github/my-repos/
+cd ~/Documents/github/my-repos/
+git clone git@github.com:brightly-ccv/driver-setup.git
+cd driver-setup
+ansible-playbook --connection=local -i localhost, -bK driver-setup.yml
